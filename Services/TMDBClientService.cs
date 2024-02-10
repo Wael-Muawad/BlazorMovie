31e7a4cb0eeaf3b7992f3d8cf711283d73e13350
wael <wael.muawad@gmail.com>
2024-02-10 14:34:18 +0300

    added the movie card component

--- Services/TMDBClientService.cs
+++ Services/TMDBClientService.cs
@@ -1,13 +1,45 @@
-﻿namespace BlazorMovie.Services
+﻿using BlazorMovie.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace BlazorMovie.Services
 {
     public class TMDBClientService
     {
         private readonly HttpClient _httpClient;
 
-        public TMDBClientService(HttpClient httpClient)
+        public TMDBClientService(HttpClient httpClient, IConfiguration config)
         {
             _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+            _httpClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string apikey = config.GetSection("apikey").Value
+                ?? throw new Exception("TMDB key was not found");
+
+            _httpClient.DefaultRequestHeaders.Authorization = 
+                new AuthenticationHeaderValue("Bearer", apikey);
         }
+
+
+        public async Task<PopularMoviePagedResponse> GetPopularMovies()
+        {
+            try
+            {
+                var result = await _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>("movie/popular");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        
+        
+        }
+
 
 
     }
