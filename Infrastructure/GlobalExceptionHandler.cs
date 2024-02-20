b4725828ba8d90b532ada945d9196c33ff7e49a8
wael <wael.muawad@gmail.com>
2024-02-21 00:15:00 +0300

    add global exception handler

---
+++ Infrastructure/GlobalExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorMovie.Infrastructure
+{
+    public class GlobalExceptionHandler : ErrorBoundary//: IExceptionHandler
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment _hostEnvironment { get; set; }
+
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            if (_hostEnvironment.IsDevelopment())
+            {
+                return base.OnErrorAsync(exception);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Services/TMDBClientService.cs
+++ Services/TMDBClientService.cs
@@ -18,38 +18,23 @@ namespace BlazorMovie.Services
             string apikey = config.GetSection("apikey").Value
                 ?? throw new Exception("TMDB key was not found");
 
-            _httpClient.DefaultRequestHeaders.Authorization = 
+            _httpClient.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", apikey);
         }
 
 
         public async Task<PopularMoviePagedResponse?> GetPopularMovies(int pageNum = 1)
         {
-            try
-            {
-                if (pageNum < 1) pageNum = 1;
-                else if (pageNum > 500) pageNum = 500;
+            if (pageNum < 1) pageNum = 1;
+            else if (pageNum > 500) pageNum = 500;
+            throw new IndexOutOfRangeException();
+            return await _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>($"movie/popular?page={pageNum}");
 
-                return await _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>($"movie/popular?page={pageNum}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }    
         }
 
         public async Task<MovieDetails?> GetMovieById(int id)
         {
-            try
-            {
-                return await _httpClient.GetFromJsonAsync<MovieDetails>($"movie/{id}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
+            return await _httpClient.GetFromJsonAsync<MovieDetails>($"movie/{id}");
         }
 
     }
